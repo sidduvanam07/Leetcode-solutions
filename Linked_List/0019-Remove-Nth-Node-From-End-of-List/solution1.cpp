2aa5b62ddbbe4a2223104726e970bd4a3026e943
sidduvanam07 <sidduvanam07@gmail.com>
2026-07-08 19:22:47 +0530

    Create solution1.cpp

---
+++ Linked_List/0019-Remove-Nth-Node-From-End-of-List/solution1.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        int lenA = 0, lenB = 0;
+        // int lenB = 0;
+
+        while(a != NULL){
+            lenA++;
+            a = a->next;
+        }
+         while(b != NULL){
+            lenB++;
+            b = b->next;
+        }
+
+        a = headA;
+        b = headB;
+        while(lenA > lenB){
+            a = a->next;
+            lenA--;
+        }
+        while(lenB > lenA){
+            b = b->next;
+            lenB--;
+        }
+
+        while(a != b){
+            a = a->next;
+            b = b->next;
+        }
+
+        return a;
+
+    }
+};
